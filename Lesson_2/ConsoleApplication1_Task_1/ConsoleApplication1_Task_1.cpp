deb5554429c39077c4592aacc3bf95dc55d412da
Alexander Zhuravlev <a-798@mail.ru>
2024-03-17 04:48:30 +0300

    add Lesson_2

---
+++ Lesson_2/ConsoleApplication1_Task_1/ConsoleApplication1_Task_1.cpp
@@ -0,0 +1,61 @@
+﻿#include <string>
+#include <iostream>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text(text) {}
+    Text* text;
+};
+
+
+class Paragraph : public DecoratedText
+{
+public:
+    explicit Paragraph(Text* text) : DecoratedText(text) {}
+    void render (const std::string& data) const override  {
+        std::cout << "<p>";
+        text->render(data);
+        std::cout << "</p>";
+    }
+};
+
+
+class Reversed : public DecoratedText
+{
+public:
+    explicit Reversed(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const override {
+        std::string temp_string = data; //нужно создать временный неконстантный объект, иначе reverse не сработает
+        std::reverse(temp_string.begin(), temp_string.end());
+        text->render(temp_string);
+    }
+};
+
+class Link : public DecoratedText
+{
+public:
+    explicit Link(Text* text) : DecoratedText(text) {}
+    void render(const std::string link, const std::string data) //т.к. сигнатура отличается, то не пишем override
+    {
+        std::cout << "<a href=" << link << ">";
+        text->render(data);
+        std::cout << "</a>";
+    }
+};
+
+
+int main() {
+    auto text_block1 = new Reversed(new Paragraph(new Text()));
+    text_block1->render("Hello world");
+    std::cout << std::endl;
+    auto text_block2 = new Link(new Text());
+    text_block2->render("netology.ru", "Hello world");
+}
---
+++ Lesson_2/ConsoleApplication1_Task_2/ConsoleApplication1_Task_2.cpp
@@ -0,0 +1,47 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "value: " + key;
+    }
+};
+
+
+class OneShotDB : VeryHeavyDatabase {
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots) : real_db_(real_object), counter(shots) {}
+    
+    std::string GetData(const std::string& key) noexcept 
+    {
+        if (cache_.find(key) == cache_.end() && counter > 0) {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+            counter--;
+        }
+        else if (counter > 0) {
+            std::cout << "Get from cache\n";
+            counter--;
+        }
+        else
+            return "error";
+        return cache_.at(key);
+    }
+
+private:
+    std::map<std::string, std::string> cache_;
+    VeryHeavyDatabase* real_db_;
+    size_t counter;
+};
+
+
+int main() {
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2); // с помощью std::addressof передаем адрес в констурктор CacheProxyDB
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    return 0;
+}
